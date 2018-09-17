52366481dc171077d3378d53062480c6e9e39681
Serge Metellus <surge@linux-0lmj.attlocal.net>
2018-09-17 18:31:33 -0400

    sprint1 done

---
+++ flaskapp.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Home Page</h1>'
+
+@app.route('/about')
+def about():
+	return '<h1>About Page</h1>'
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+
+if __name__ == '__main__':
+	app.run(debug=True)
