7cc8c62c3596521d5535adba349bdc855bacaa65
torte007 <torte007@fiu.edu>
2018-09-27 18:20:34 -0400

    fix database issues and add rating to comments

--- geektext/__init__.py
+++ geektext/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
