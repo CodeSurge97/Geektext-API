054b2c70a54832da30bd21ad995eb5b686a56f45
torte007 <torte007@fiu.edu>
2018-09-28 05:15:01 -0400

    add templates for author and add comments for each book

--- geektext/routes.py
+++ geektext/routes.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, make_response, jsonify, url_for, send_
 from geektext import app, db
 from geektext.models import *
 import io
+from flask import request
 
+
+"""
 @app.route('/books')
 def home():
     books = Book.query.order_by(Book.date_pub).all()
@@ -22,23 +25,43 @@ def home():
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
+"""
 
+@app.route('/book/<int:isbn>')
+def book_page(isbn):
+    b = Book.query.filter_by(isbn=isbn)
+    return render_template('book.html', book=b.first_or_404())
+
+@app.route('/')
+def home():
+    books = Book.query.all()
+    return render_template('home.html', books=books)
+
+
+@app.route('/author/<int:id>')
+def author_page(id):
+    a = Author.query.get_or_404(id)
+    return render_template('author.html', author=a)
+
+
+
+"""
 @app.route('/books/<isbn>')
 def book_page(book_title):
 
     target = Book.query.filter(Book.title.like('%'+book_title+'%'))
     return render_template('book.html', book=target.first())
 
+"""
 
-
-    """
-    b = { 'title': book.title,
-    'isbn': book.isbn,
-    'date_pub' = book.date_pub,
-    'genre': book.genre,
-    'rating': book.rating,
-    'price' : book.price,
-    'img' : book.img,
-    'pub_info' : book.pub_info,
-    'book_description': book.book_description}
-    """
+"""
+b = { 'title': book.title,
+'isbn': book.isbn,
+'date_pub' = book.date_pub,
+'genre': book.genre,
+'rating': book.rating,
+'price' : book.price,
+'img' : book.img,
+'pub_info' : book.pub_info,
+'book_description': book.book_description}
+"""
